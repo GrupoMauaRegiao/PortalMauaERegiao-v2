6789bde6152a82490f1cabfcdbb3dc325a181791
marcker <marckfree@gmail.com>
2014-06-05 11:37:32 -0300

    Adiciona tipo de post pub_190_x_193 e loop para e exibir este conteúdo

--- wordpress/wp-content/themes/portal-maua-e-regiao/index.php
+++ wordpress/wp-content/themes/portal-maua-e-regiao/index.php
@@ -425,11 +425,33 @@
 
             <!-- ///////////////// PUBLICIDADE ////////////////// -->
             <section class="publicidade">
-              <a href="#" title="">
-                <section class="imagem">
-                  <img src="#" alt="">
-                </section>
-              </a>
+              <?php query_posts("orderby=rand&showposts=1&post_type=pub_190_x_193"); ?>
+              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                <a target="_blank" href="<?php echo get_post_meta($post -> ID, "link", true); ?>" title="Publicidade &#8212; <?php echo get_post_meta($post -> ID, "link", true); ?>">
+
+                  <?php if (get_post_meta($post -> ID, "imagem", true)): ?>
+
+                    <section class="imagem">
+                      <img src="<?php echo get_post_meta($post -> ID, "imagem", true); ?>" alt="">
+                    </section>
+
+                  <?php elseif (get_post_meta($post -> ID, "object", true)): ?>
+
+                    <section class="object publicidade">
+                      <object data="<?php echo get_post_meta($post -> ID, "object", true); ?>"
+                              type="application/x-shockwave-flash">
+                        <param name="movie" value="<?php echo get_post_meta($post -> ID, "object", true); ?>">
+                        <param name="quality" value="high">
+                      </object>
+                    </section>
+
+                  <?php endif; ?>
+                </a>
+              <?php endwhile; else: ?>
+
+                <p>ANUNCIE AQUI</p>
+
+              <?php endif; ?>
             </section>
             <!-- ///////////////// close PUBLICIDADE ////////////////// -->
           </section>
