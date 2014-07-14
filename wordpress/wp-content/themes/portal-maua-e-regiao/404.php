5d8729e0687e6c6266554e03ce003a5b50df9712
marcker <marckfree@gmail.com>
2014-07-14 11:11:03 -0300

    Adiciona página 404.php

--- wordpress/wp-content/themes/portal-maua-e-regiao/404.php
+++ wordpress/wp-content/themes/portal-maua-e-regiao/404.php
@@ -1,7 +1,32 @@
 <?php get_header(); ?>
+<?php wp_reset_query(); ?>
 
-<section>
-  Página não encontrada.
+<section class="ultimas-noticias">
+  <section class="layout-ultimas-noticias">
+    <section class="conteudo">
+      <section class="principal">
+        <section class="header">
+          <section class="titulo categoria">
+            Página não encontrada
+          </section>
+          <section class="decoracao">
+            <section class="linha-grossa"></section>
+            <section class="linha-fina"></section>
+          </section>
+        </section>
+
+        <section class="erro-chamada">
+          <section>
+            <section class="icone-erro"></section>
+            Desculpe-nos! Mas a página que você procurou não existe.
+          </section>
+        </section>
+      </section>
+
+      <?php get_sidebar(); ?>
+
+    </section>
+  </section>
 </section>
 
 <?php get_footer(); ?>
