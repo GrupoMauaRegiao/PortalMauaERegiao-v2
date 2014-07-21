a95827159a72fa97c9fcfc7c3a94d26efe84dc05
marcker <marckfree@gmail.com>
2014-07-21 10:21:02 -0300

    Adiciona página de erro 500 e página para Fatos e Fotos (com galeria)

---
+++ wordpress/wp-content/themes/portal-maua-e-regiao/500.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<?php wp_reset_query(); ?>
+
+<section class="ultimas-noticias">
+  <section class="layout-ultimas-noticias">
+    <section class="conteudo">
+      <section class="principal">
+        <section class="header">
+          <section class="titulo categoria">
+            Erro no servidor
+          </section>
+          <section class="decoracao">
+            <section class="linha-grossa"></section>
+            <section class="linha-fina"></section>
+          </section>
+        </section>
+
+        <section class="erro-chamada">
+          <section>
+            <section class="icone-erro"></section>
+            Desculpe-nos! A página que você procurou não pôde ser acessada.
+          </section>
+        </section>
+      </section>
+
+      <?php get_sidebar(); ?>
+
+    </section>
+  </section>
+</section>
+
+<?php get_footer(); ?>
