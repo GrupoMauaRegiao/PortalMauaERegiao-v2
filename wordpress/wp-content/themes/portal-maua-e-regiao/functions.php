03bbf590f3ba7148d5a0ec50ce49efcbb7712c31
marcker <marckfree@gmail.com>
2014-12-16 11:24:32 -0200

    Ajusta paginação

--- wordpress/wp-content/themes/portal-maua-e-regiao/functions.php
+++ wordpress/wp-content/themes/portal-maua-e-regiao/functions.php
@@ -3812,13 +3812,14 @@ function query_todas_noticias($categoria = null) {
 
 function query_agenda() {
     $query = array(
-            "meta_key"  => "data",
-            "nopaging"  => true,
-            "orderby"   => "meta_value",
-            "order"     => "asc",
-            "post_type" => array(
+            "meta_key"      => "data",
+            "nopaging"      => true,
+            "orderby"       => "meta_value",
+            "order"         => "asc",
+            "past_per_page" => 31,
+            "post_type"     => array(
                                    "agenda"
-                           )
+                               )
     );
 
     return $query;
